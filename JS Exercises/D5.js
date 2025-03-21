76c2bd06427dfeb22c1611ffb23f8af250f44244
Ivan Croce <ivan.croce.it@gmail.com>
2025-03-21 15:54:40 +0000

    adds 2 more solutions

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -78,23 +78,35 @@ const newCar = {
 };
 
 cars.push(newCar);
-console.log(cars);
+console.log("Before: ", cars);
 
 for (let i = 0; i < cars.length; i++) {
   cars[i].trims.pop();
 }
 
-console.log(cars);
+console.log("After: ", cars);
 
 /* ESERCIZIO 7
-    Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
+Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
 const justTrims = [];
+
+for (let i = 0; i < cars.length; i++) {
+  justTrims.push(cars[i].trims[0]);
+}
+
+console.log(justTrims);
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+for (let i = 0; i < cars.length; i++) {
+  if (cars[i].color.startsWith(`b`)) console.log("Fizz");
+  else {
+    console.log("Buzz");
+  }
+}
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
